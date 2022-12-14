1c38d6efb0f30d82368cf62fd9f0c96c280c4d53
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2022-12-14 14:13:26 +0100

    Initial Python 3 changes
    Fixes imports and Python 3 specific function calls. Removes the
    now unecessary unicodecsv.py class. Adds Black formatting.

--- DetectEmpties.py
+++ DetectEmpties.py
@@ -1,91 +1,105 @@
 import sys
 from droidcsvhandlerclass import *
 
-#Create CSV list, DROIDLIST
-#GRAB 'ALL' PARENT_IDs FROM DROIDLIST
-#LOOK FOR UNIQUENESS IN PARENT_ID LIST
-#IF FOLDER 'ID' IS NEVER A 'PARENT_ID' IT IS EMPTY
-#SIMULATE REMOVAL OF THE FOLDER AND THEREFORE ID FOR THAT FOLDER
-#IF WE HAVE JUST ONE EMPTY FOLDER REPEAT TO SEE IF A NEXT FOLDER BECOMES EMPTY
-#ETC. RECURSE.
+# Create CSV list, DROIDLIST
+# GRAB 'ALL' PARENT_IDs FROM DROIDLIST
+# LOOK FOR UNIQUENESS IN PARENT_ID LIST
+# IF FOLDER 'ID' IS NEVER A 'PARENT_ID' IT IS EMPTY
+# SIMULATE REMOVAL OF THE FOLDER AND THEREFORE ID FOR THAT FOLDER
+# IF WE HAVE JUST ONE EMPTY FOLDER REPEAT TO SEE IF A NEXT FOLDER BECOMES EMPTY
+# ETC. RECURSE.
+
 
 class DetectEmpties:
-   #Consider merging into DROIDCSVHANDLER class...
+    # Consider merging into DROIDCSVHANDLER class...
 
-   emptylist = []
-   blacklist = False
+    emptylist = []
+    blacklist = False
 
-   #simulate deletion of file structure...
-   def recurse_delete(self, droidlist, folderIDlist):
-      empties = False
+    # simulate deletion of file structure...
+    def recurse_delete(self, droidlist, folderIDlist):
+        empties = False
 
-      #if folder does not appear in PARENT_ID list it is empty
-      parentIDs = []
-      for row in droidlist:
-         parentIDs.append(row['PARENT_ID'])
+        # if folder does not appear in PARENT_ID list it is empty
+        parentIDs = []
+        for row in droidlist:
+            parentIDs.append(row["PARENT_ID"])
 
-      uniqueIDs = set(parentIDs)
-      for id in folderIDlist:
-         if id not in uniqueIDs:
-            for row in list(droidlist):
-               if row['ID'] == id:
-                  empties = True
-                  self.emptylist.append(row['FILE_PATH'])
-                  droidlist.remove(row)   #remove empty folder
-                  for folderid in list(folderIDlist):
-                     if row['ID'] == folderid:
-                        folderIDlist.remove(folderid)
-                        
-      if empties == True:
-         self.recurse_delete(droidlist, folderIDlist)
+        uniqueIDs = set(parentIDs)
+        for id in folderIDlist:
+            if id not in uniqueIDs:
+                for row in list(droidlist):
+                    if row["ID"] == id:
+                        empties = True
+                        self.emptylist.append(row["FILE_PATH"])
+                        droidlist.remove(row)  # remove empty folder
+                        for folderid in list(folderIDlist):
+                            if row["ID"] == folderid:
+                                folderIDlist.remove(folderid)
 
-   def removeblacklistitems(self, droidlist, folderIDlist, pathblacklist, puidblacklist, zerobytefiles):
-      if pathblacklist:
-         sys.stderr.write("Note: Using file path blacklists.\n")
-         for file in pathblacklist:
+        if empties == True:
+            self.recurse_delete(droidlist, folderIDlist)
+
+    def removeblacklistitems(
+        self, droidlist, folderIDlist, pathblacklist, puidblacklist, zerobytefiles
+    ):
+        if pathblacklist:
+            sys.stderr.write("Note: Using file path blacklists.\n")
+            for file in pathblacklist:
+                for item in list(droidlist):
+                    if item["FILE_PATH"] == file:
+                        droidlist.remove(item)
+                        break
+        if puidblacklist:
+            sys.stderr.write("Note: Using puid blacklists.\n")
+            for puid in puidblacklist:
+                for item in list(droidlist):
+                    if (
+                        item["PUID"] == puid
+                        and item["METHOD"] == "Signature"
+                        or item["METHOD"] == "Container"
+                    ):
+                        droidlist.remove(item)
+        if zerobytefiles:
+            sys.stderr.write("Note: Using zero byte file blacklists.\n")
             for item in list(droidlist):
-               if item['FILE_PATH'] == file:
-                  droidlist.remove(item)
-                  break
-      if puidblacklist:
-         sys.stderr.write("Note: Using puid blacklists.\n")
-         for puid in puidblacklist:
-            for item in list(droidlist):
-               if item['PUID'] == puid and item['METHOD'] == 'Signature' or item['METHOD'] == 'Container':
-                  droidlist.remove(item)
-      if zerobytefiles:
-         sys.stderr.write("Note: Using zero byte file blacklists.\n")
-         for item in list(droidlist):
-            if item['SIZE'] == '0':
-               droidlist.remove(item)
+                if item["SIZE"] == "0":
+                    droidlist.remove(item)
 
-   def createFolderList(self, droidlist):
-      folderIDlist = []
-      for row in droidlist:
-         if row['TYPE'] == 'Folder':
-            folderIDlist.append(row['ID'])
-      return folderIDlist
-      
-   #primary function, create a full list from DROID CSV
-   #create a list of IDs belonging to just folders...
-   def detectEmpties(self, csv, pathblacklist=False, puidblacklist=False, zerobytefiles=False):
-      droidcsv = droidCSVHandler()
-      droidlist = droidcsv.readDROIDCSV(csv)
-      folderIDlist = self.createFolderList(droidlist)
-      
-      if pathblacklist or puidblacklist or zerobytefiles:
-         self.blacklist = True
-         self.removeblacklistitems(droidlist, folderIDlist, pathblacklist, puidblacklist, zerobytefiles)
-      
-      self.recurse_delete(droidlist, folderIDlist)
-      self.outputEmptyList()
+    def createFolderList(self, droidlist):
+        folderIDlist = []
+        for row in droidlist:
+            if row["TYPE"] == "Folder":
+                folderIDlist.append(row["ID"])
+        return folderIDlist
 
-   def outputEmptyList(self):
-      outputtext = " or its siblings contains zero FILE-objects." + "\n"
-      if self.blacklist:
-         outputtext = " or its siblings contains or will contain no FILE-objects once blacklist destructions have been implemented." + "\n"
-      if len(self.emptylist) > 0:
-         for file in self.emptylist:
-            sys.stdout.write(file + outputtext) 
-      else:
-         sys.stderr.write("There are no empty folders." + "\n")
+    # primary function, create a full list from DROID CSV
+    # create a list of IDs belonging to just folders...
+    def detectEmpties(
+        self, csv, pathblacklist=False, puidblacklist=False, zerobytefiles=False
+    ):
+        droidcsv = droidCSVHandler()
+        droidlist = droidcsv.readDROIDCSV(csv)
+        folderIDlist = self.createFolderList(droidlist)
+
+        if pathblacklist or puidblacklist or zerobytefiles:
+            self.blacklist = True
+            self.removeblacklistitems(
+                droidlist, folderIDlist, pathblacklist, puidblacklist, zerobytefiles
+            )
+
+        self.recurse_delete(droidlist, folderIDlist)
+        self.outputEmptyList()
+
+    def outputEmptyList(self):
+        outputtext = " or its siblings contains zero FILE-objects." + "\n"
+        if self.blacklist:
+            outputtext = (
+                " or its siblings contains or will contain no FILE-objects once blacklist destructions have been implemented."
+                + "\n"
+            )
+        if len(self.emptylist) > 0:
+            for file in self.emptylist:
+                sys.stdout.write(file + outputtext)
+        else:
+            sys.stderr.write("There are no empty folders." + "\n")
--- detect-empty-folders.py
+++ detect-empty-folders.py
@@ -1,71 +1,80 @@
-﻿#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
+﻿"""Detect empty folders script using a DROID file format export.
+"""
 
 import os
 import sys
 import argparse
-import ConfigParser
+import configparser as ConfigParser
 from DetectEmpties import *
 
+
 def readconfig(configfile):
-   paths = False
-   puids = False
-   zerobytes = False
+    paths = False
+    puids = False
+    zerobytes = False
 
-   config = ConfigParser.ConfigParser()
-   config.read(configfile)
-   if config.has_section('blacklist'):
-      if config.has_option('blacklist', 'filepaths'):
-         configpaths = config.get('blacklist', 'filepaths')
-         if configpaths.lower() != 'false':
-            paths = configpaths.split(',')
-      if config.has_option('blacklist', 'puids'):
-         configpuids = config.get('blacklist', 'puids')
-         if configpuids.lower() != 'false':
-            puids = configpuids.split(',')
-      if config.has_option('blacklist', 'zerobytefiles'):
-         zerobytefiles = config.get('blacklist', 'zerobytefiles')
-         if zerobytefiles.lower() == 'false' or zerobytefiles == '':
-            zerobytes = False
-         else:
-            zerobytes = True
+    config = ConfigParser.ConfigParser()
+    config.read(configfile)
+    if config.has_section("blacklist"):
+        if config.has_option("blacklist", "filepaths"):
+            configpaths = config.get("blacklist", "filepaths")
+            if configpaths.lower() != "false":
+                paths = configpaths.split(",")
+        if config.has_option("blacklist", "puids"):
+            configpuids = config.get("blacklist", "puids")
+            if configpuids.lower() != "false":
+                puids = configpuids.split(",")
+        if config.has_option("blacklist", "zerobytefiles"):
+            zerobytefiles = config.get("blacklist", "zerobytefiles")
+            if zerobytefiles.lower() == "false" or zerobytefiles == "":
+                zerobytes = False
+            else:
+                zerobytes = True
 
-   return paths, puids, zerobytes
+    return paths, puids, zerobytes
+
 
 def main():
 
-   # Usage: --csv          [droid report]
-   #        --blfilepath   [path of objects selected for deletion]
-   #        --blpuid       [puids we've blacklisted]
-   #        --blzeros      [elect to delete zero byte files, non-records]
-   #
-   # Handle command line arguments for the script
-   parser = argparse.ArgumentParser(description='Detect empty folders in DROID CSV Reports.')
+    # Usage: --csv          [droid report]
+    #        --blfilepath   [path of objects selected for deletion]
+    #        --blpuid       [puids we've blacklisted]
+    #        --blzeros      [elect to delete zero byte files, non-records]
+    #
+    # Handle command line arguments for the script
+    parser = argparse.ArgumentParser(
+        description="Detect empty folders in DROID CSV Reports."
+    )
 
-   # Just the one argument here, DROID csv... 
-   parser.add_argument('--csv', help='Single DROID CSV to read.', default=False, required=True)
-   parser.add_argument('--blacklist', help='Use blacklist file.', default=False, required=False)   
+    # Just the one argument here, DROID csv...
+    parser.add_argument(
+        "--csv", help="Single DROID CSV to read.", default=False, required=True
+    )
+    parser.add_argument(
+        "--blacklist", help="Use blacklist file.", default=False, required=False
+    )
 
-   if len(sys.argv)==1:
-      parser.print_help()
-      sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
-   #	Parse arguments into namespace object to reference later in the script
-   global args
-   args = parser.parse_args()
-   
-   blacklist = args.blacklist
-   
-   if args.csv:
-      empty = DetectEmpties()
-      if blacklist:
-         blacklist = readconfig(blacklist)
-         empty.detectEmpties(args.csv, blacklist[0], blacklist[1], blacklist[2])
-      else:
-         empty.detectEmpties(args.csv)
-   else:
-      parser.print_help()
-      sys.exit(1)
+    # 	Parse arguments into namespace object to reference later in the script
+    global args
+    args = parser.parse_args()
+
+    blacklist = args.blacklist
+
+    if args.csv:
+        empty = DetectEmpties()
+        if blacklist:
+            blacklist = readconfig(blacklist)
+            empty.detectEmpties(args.csv, blacklist[0], blacklist[1], blacklist[2])
+        else:
+            empty.detectEmpties(args.csv)
+    else:
+        parser.print_help()
+        sys.exit(1)
+
 
 if __name__ == "__main__":
-   main()
+    main()
